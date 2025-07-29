a2fa4f92fa70ada08349fa03177a530f11b10f53
willkersv <willkersv@gmail.com>
2025-07-29 11:32:37 -0300

    add do projeto do gitHub

---
+++ backend/src/test/java/com/oficina/nikima/NikimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oficina.nikima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NikimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
